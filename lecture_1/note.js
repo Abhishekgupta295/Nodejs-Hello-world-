4b5417d84ba0271eab2d703e728302f969cd4119
Abhishekgupta295 <guptaabhi2952nd@gmail.com>
2025-12-06 14:24:28 +0530

    First commit for this Project

---
+++ lecture_1/note.js
@@ -0,0 +1,6 @@
+console.log("This is a note file for lecture 1");
+var age = 25;
+const addNum = function (a, b) {
+  return a + b;
+};
+module.exports = { age, addNum };
---
+++ lecture_1/server.js
@@ -0,0 +1,74 @@
+const express = require('express');
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Hello World ! Welcome to our server! wth nodemon')
+})
+app.get('/chicken', (req, res) => {
+  res.send('i love chicken!')
+})
+app.get('/dosa', (req, res) => {
+  let customizedDosa = {
+    type: 'Masala Dosa',
+    size: 'Large',
+    quantity: 2,
+  };
+  // res.send('i love Dosa!welcome to dosa world');
+  res.send(customizedDosa);
+})
+
+app.listen(3000, ( )=> {
+  console.log('Server is running on port 3000!');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const notes = require("./note.js"); 
+// console.log("our server is starting");
+// // function add(x, y) {
+// //   return x + y;
+// // }
+// // console.log("2 + 3 =", add(2, 23));
+// var fs = require("fs");
+// var os = require("os");
+// var _ = require("lodash");
+
+// var info = os.userInfo();
+// var type = os.type();
+// console.log("User info:", info);
+// console.log("OS Type:", type);
+
+// fs.appendFile("test.txt", "Hello world \n", () => {
+//   console.log("File written");
+// });
+
+// var age = notes.age;
+// var result = notes.addNum(5, 10);
+// console.log("Age from note.js:", age);
+// console.log("Add 5 + 10 using addNum from note.js:", result);
+
+// var data = ["person", "person", 1, 2, 1, 2, "name", "age", "2"];
+
+// var filter = _.uniq(data);
+// var result22 = _.isString(7);
+
+// console.log(filter);
+// console.log("Is 'Hello World' a string?", result22);
