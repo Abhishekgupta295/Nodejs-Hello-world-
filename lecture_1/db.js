74f2c2d4dbcdaf16ba57f6118c0372ff4ac8ec36
Abhishekgupta295 <guptaabhi2952nd@gmail.com>
2025-12-20 18:53:44 +0530

    lec-4 complete now

--- lecture_1/db.js
+++ lecture_1/db.js
@@ -1,11 +1,21 @@
 const mongoose = require('mongoose');
 
-const mongoURL = 'mongodb://localhost:27017/hotels';
+const mongoURL = 'mongodb://localhost:27017/newhotels';
 
-mongoose.connect(mongoURL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(mongoURL)
 
 const db = mongoose.connection;
 
 db.on('connected', () => {
   console.log('Connected to MongoDB successfully!');
 });
+
+db.on('error', (error) => {
+  console.log('Error connecting to MongoDB', error);
+});
+
+db.on('disconnected', () => {
+  console.log('Disconnected from MongoDB');
+});
+
+module.exports = db;
--- lecture_1/server.js
+++ lecture_1/server.js
@@ -1,32 +1,86 @@
 const express = require('express');
+const db = require('./db.js');
+const Person = require('./models/person.js');
+const bodyParser = require('body-parser');
+const MenuItem = require('./models/MenuItem.js');
 
-const app = express()
+const app = express();
+
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   res.send('Hello World ! Welcome to our server! wth nodemon')
 })
-app.get('/chicken', (req, res) => {
-  res.send('i love chicken biryani!')
-})
-app.get('/dosa', (req, res) => {
-  let customizedDosa = {
-    type: 'Masala Dosa',
-    size: 'Large',
-    quantity: 2,
-  };
-  // res.send('i love Dosa!welcome to dosa world');
-  res.send(customizedDosa);
-})
+// app.get('/chicken', (req, res) => {
+//   res.send('i love chicken biryani!')
+// })
+// app.get('/dosa', (req, res) => {
+//   let customizedDosa = {
+//     type: 'Masala Dosa',
+//     size: 'Large',
+//     quantity: 2,
+//   };
+//   // res.send('i love Dosa!welcome to dosa world');
+//   res.send(customizedDosa);
+// })
 
-app.post('/items', (req, res) => {
-  res.send('This is a POST request for Dosa');
-});
+// app.post('/items', (req, res) => {
+//   res.send('This is a POST request for Dosa');
+// });
 app.listen(3000, ( )=> {
   console.log('Server is running on port 3000!');
 });
 
+//old way with callback
+// app.post('/person', (req,res) => {
 
+//   const personData = req.body; // assuming request body contains person data in JSON format coming from client in POST request
+//   const newPerson = new Person(personData); // creating new Person instance using mongoose model.
+//   newPerson.save((error, savedPerson) =>{
+//     if (error) {
+//       console.log('Error saving person:', error);
+//       return res.status(500).send('Error saving person to database');
+//     }
+//     else{
+//       console.log('Person saved successfully:', savedPerson);
+//        res.status(201).send(savedPerson);
+//     }
+   
+//   }) // saving the new person to the database
 
+// })
+
+//new way with async await
+app.post('/person', async (req,res) => {
+
+  try{
+
+    const personData = req.body; // assuming request body contains person data in JSON format coming from client in POST request
+    const newPerson = new Person(personData); // creating new Person instance using mongoose model.
+    const response = await newPerson.save();
+    res.status(200).send(response);
+    console.log('Person saved successfully:', response);
+
+  }
+  catch(error){
+    console.log('Error saving person:', error);
+    return res.status(500).send('Error saving person to database');
+  }
+
+  
+})
+
+app.get('/person', async(req,res) => {
+  try{
+      const data = await Person.find();
+      console.log('Persons data retrieved successfully:', data);
+      res.status(200).send(data);
+  }
+  catch(error){
+    console.log('Error saving person:', error);
+    return res.status(500).send('Error saving person to database');
+  }
+});
 
 
 
