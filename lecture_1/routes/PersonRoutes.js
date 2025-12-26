76cb28c9d5339a84ebb5ab1d94a9c79ceeb00f1d
Abhishekgupta295 <guptaabhi2952nd@gmail.com>
2025-12-26 14:35:11 +0530

    lllll

--- lecture_1/routes/PersonRoutes.js
+++ lecture_1/routes/PersonRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const Person = require('../models/Person');
 
+
 //old way with callback
 // app.post('/person', (req,res) => {
 
